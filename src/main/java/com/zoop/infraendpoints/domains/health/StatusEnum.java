8e0d5aad354375e91eb5e35fb6182bb9147a30f0
julianohsf <juliano.fernandes@zoop.co>
2018-05-12 22:09:02 -0300

    Melhorias na configuração do endpoint de health

--- src/main/java/com/zoop/infraendpoints/domains/health/StatusEnum.java
+++ src/main/java/com/zoop/infraendpoints/domains/health/StatusEnum.java
@@ -3,7 +3,7 @@ package com.zoop.infraendpoints.domains.health;
 public enum StatusEnum {
 
     UP("All dependencies are up", 200),
-    PARTIAL("All critical dependencies are up, but there is at least one non critical dependency out of service.", 207),
+    PARTIAL("All critical dependencies are up, but there is at least one non critical dependency out of service", 207),
     FAIL("There is at least one critical dependency out of service", 503);
 
     private final String message;
--- src/main/java/com/zoop/infraendpoints/services/HealthService.java
+++ src/main/java/com/zoop/infraendpoints/services/HealthService.java
@@ -3,6 +3,7 @@ package com.zoop.infraendpoints.services;
 import com.zoop.infraendpoints.domains.health.Dependency;
 import com.zoop.infraendpoints.domains.health.Health;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -10,16 +11,35 @@ public class HealthService {
 
     private final Health health;
 
-    public HealthService(List<Dependency> dependencies) {
-        if (Objects.isNull(dependencies)) {
-            throw new IllegalArgumentException("Dependencies list could not be null");
-        }
-
+    private HealthService(List<Dependency> dependencies) {
         this.health = new Health(dependencies);
     }
 
     public Health check() {
         return health.check();
+    }
+
+    public static class Builder {
+
+        private final List<Dependency> dependencies;
+
+        private Builder() {
+            this.dependencies = new ArrayList<>();
+        }
+
+        public static Builder newInstance() {
+            return new Builder();
+        }
+
+        public Builder addDependency(Dependency dependency) {
+            this.dependencies.add(dependency);
+            return this;
+        }
+
+        public HealthService build() {
+            return new HealthService(this.dependencies);
+        }
+
     }
 
 }
