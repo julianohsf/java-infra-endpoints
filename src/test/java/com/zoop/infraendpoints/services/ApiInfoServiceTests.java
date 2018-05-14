7f8c57cde0e2817a0d53e2b2657c1e91be952211
julianohsf <juliano.fernandes@zoop.co>
2018-05-14 01:45:37 -0300

    Adicionando testes aos services

---
+++ src/test/java/com/zoop/infraendpoints/services/ApiInfoServiceTests.java
@@ -0,0 +1,48 @@
+package com.zoop.infraendpoints.services;
+
+import com.zoop.infraendpoints.domains.apiinfo.ApiInfo;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+@RunWith(JUnit4.class)
+public class ApiInfoServiceTests {
+
+    @Test
+    public void searchManifestFailedByFileName() throws Exception {
+        try {
+            ApiInfoService service = new ApiInfoService("META-INF/MANIFEST.MF", "test-application-2");
+            fail("Expected to throws IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            assertEquals("No manifest files founded at META-INF/MANIFEST.MF with applicationName test-application-2", e.getMessage());
+        }
+    }
+
+    @Test
+    public void searchManifestFailedByApplicationName() throws Exception {
+        try {
+            ApiInfoService service = new ApiInfoService("META-INF/MANUFACTURE.MF", "test-application");
+            fail("Expected to throws IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            assertEquals("No manifest files founded at META-INF/MANUFACTURE.MF with applicationName test-application", e.getMessage());
+        }
+    }
+
+    @Test
+    public void searchManifest() throws Exception {
+        ApiInfoService service = new ApiInfoService("META-INF/MANIFEST.MF", "test-application");
+        ApiInfo apiInfo = service.getInfo();
+        assertEquals("test-application", apiInfo.getApplicationName());
+        assertEquals("Zoop Builder Tool", apiInfo.getCreatedBy());
+        assertEquals("1.8.0_161", apiInfo.getBuildJdk());
+        assertEquals("2.0.2.RELEASE", apiInfo.getSpringBootVersion());
+        assertEquals("20180512-7", apiInfo.getBuildNumber());
+        assertEquals("1.0.0", apiInfo.getVersion());
+    }
+
+}
---
+++ src/test/java/com/zoop/infraendpoints/services/HealthServiceTests.java
@@ -0,0 +1,32 @@
+package com.zoop.infraendpoints.services;
+
+import com.zoop.infraendpoints.domains.health.Dependency;
+import com.zoop.infraendpoints.domains.health.Health;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(JUnit4.class)
+public class HealthServiceTests {
+
+    @Test
+    public void checkSuccess() {
+        HealthService service = buildService(new Dependency("Success", Boolean.TRUE, () -> Boolean.TRUE));
+
+        Health health = service.check();
+        assertEquals("UP", health.getStatus());
+    }
+
+    private HealthService buildService(Dependency... dependencies) {
+        HealthService.Builder builder = HealthService.Builder.newInstance();
+
+        for (Dependency dependency: dependencies) {
+            builder.addDependency(dependency);
+        }
+
+        return builder.build();
+    }
+
+}
